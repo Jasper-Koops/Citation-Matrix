5d929fb0d03ca89bba247a215109093efcddc392
Jasper Koops <jasperkoops91@gmail.com>
2020-01-11 16:55:08 +0100

    Created factories + tests

--- database/factories.py
+++ database/factories.py
@@ -13,6 +13,7 @@ class AuthorFactory(factory.DjangoModelFactory):
     class Meta:
         model = database_models.Author
 
+    is_dummy_data = True
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
 
@@ -23,20 +24,12 @@ class AuthorFactory(factory.DjangoModelFactory):
             return fake.first_name()
         return None
 
-    @factory.lazy_attribute
-    def initials_of_first_names(self) -> str:
-        """ Fetch the initials from the first name and optional middle name """
-        first_name_initials: str = self.first_name[0].upper()
-        if self.middle_name:
-            middle_name_initials: str = self.middle_name[0].upper()
-            return "{} {}".format(first_name_initials, middle_name_initials)
-        return first_name_initials
-
 
 class PublisherFactory(factory.DjangoModelFactory):
     class Meta:
         model = database_models.Publisher
 
+    is_dummy_data = True
     name = factory.Faker("company")
     city = factory.Faker("city")
 
@@ -45,19 +38,53 @@ class JournalFactory(factory.DjangoModelFactory):
     class Meta:
         model = database_models.Journal
 
+    is_dummy_data = True
     name = factory.Faker("company")
+    journal_publisher = factory.SubFactory(PublisherFactory)
 
 
+# FIXME - dat property ding hier toepassen
 class SourceFactory(factory.DjangoModelFactory):
     class Meta:
         model = database_models.Source
 
+    is_dummy_data = True
     title = factory.Faker("sentence")
-    authors = None
-    publisher = None
+    source_publisher = None
     year_of_publication = factory.LazyFunction(
         lambda: random.randint(1950, 2020)
     )
-    journal = factory.SubFactory(JournalFactory)
-    journal_page_range_start = None
-    journal_page_range_end = None
+    type = "AR"
+    source_journal = factory.SubFactory(JournalFactory)
+
+    @factory.lazy_attribute
+    def journal_page_range_start(self) -> int:
+        return random.randint(1, 100)
+
+    @factory.lazy_attribute
+    def journal_page_range_end(self) -> int:
+        article_length: int = random.randint(1, 30)
+        return self.journal_page_range_start + article_length
+
+    @factory.post_generation
+    def authors(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of seasons were passed in, use them
+            for a in extracted:
+                self.authors.add(a)
+
+    class Params:
+        article = factory.Trait(
+            type="AR",
+            source_publisher=None,
+            source_journal=factory.SubFactory(JournalFactory),
+        )
+        book = factory.Trait(
+            type="BK",
+            source_publisher=factory.SubFactory(PublisherFactory),
+            source_journal=None,
+        )
--- database/tests/testmodels.py
+++ database/tests/testmodels.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
 
-from database.factories import AuthorFactory, JournalFactory, PublisherFactory
-from database.models import Author, Journal, Publisher
+from database.factories import (
+    AuthorFactory,
+    JournalFactory,
+    PublisherFactory,
+    SourceFactory,
+)
+from database.models import Author, Journal, Publisher, Source
 
 
 class TestAuthorModel(TestCase):
@@ -10,9 +15,13 @@ class TestAuthorModel(TestCase):
 
     def test_init(self) -> None:
         self.assertTrue(self.author)
-        self.assertTrue(
-            self.author.first_name[0] in self.author.initials_of_first_names
+        self.assertTrue(self.author.is_dummy_data)
+
+    def test_initials_of_first_and_middle_names_property(self) -> None:
+        author: Author = AuthorFactory(
+            first_name="Jan Peter", middle_name="Fred Arnold"
         )
+        self.assertEqual(author.initials_of_first_and_middle_names, "J P F A")
 
 
 class TestPublisherModel(TestCase):
@@ -21,6 +30,7 @@ class TestPublisherModel(TestCase):
 
     def test_init(self) -> None:
         self.assertTrue(self.publisher)
+        self.assertTrue(self.publisher.is_dummy_data)
 
 
 class TestJournalModel(TestCase):
@@ -29,3 +39,69 @@ class TestJournalModel(TestCase):
 
     def test_init(self) -> None:
         self.assertTrue(self.journal)
+        self.assertTrue(self.journal.is_dummy_data)
+
+
+class TestSourceModel(TestCase):
+    def setUp(self) -> None:
+        self.source: Source = SourceFactory()
+
+    def test_init(self) -> None:
+        self.assertTrue(self.source)
+        self.assertTrue(self.source.is_dummy_data)
+
+    def test_book_trait(self) -> None:
+        article: Source = SourceFactory(article=True)
+        self.assertEqual(article.type, "AR")
+        self.assertEqual(article.source_publisher, None)
+        self.assertNotEqual(article.source_journal, None)
+
+    def test_article_trait(self) -> None:
+        book: Source = SourceFactory(book=True)
+        self.assertEqual(book.type, "BK")
+        self.assertNotEqual(book.source_publisher, None)
+        self.assertEqual(book.source_journal, None)
+
+    def test_get_publisher_property_for_books(self) -> None:
+        publisher: Publisher = PublisherFactory()
+        book: Source = SourceFactory(book=True, source_publisher=publisher)
+        self.assertEqual(book.get_publisher, publisher)
+
+    def test_get_publisher_property_for_articles(self) -> None:
+        publisher: Publisher = PublisherFactory()
+        journal: Journal = JournalFactory(journal_publisher=publisher)
+        article: Source = SourceFactory(article=True, source_journal=journal)
+        self.assertEqual(article.get_publisher, publisher)
+
+    def test_save_journal_page_check(self) -> None:
+        with self.assertRaisesMessage(
+            ValueError, "Invalid page range! Start is placed after the end!"
+        ):
+            SourceFactory(journal_page_range_start=5, journal_page_range_end=3)
+
+    def test_save_book_journal_check(self) -> None:
+        with self.assertRaisesMessage(
+            ValueError,
+            "Source with type 'book' cannot be linked to a journal!",
+        ):
+            SourceFactory(book=True, source_journal=JournalFactory())
+
+    def test_save_book_publisher_check(self) -> None:
+        with self.assertRaisesMessage(
+            ValueError, "Source with type 'book' has no publisher set!"
+        ):
+            SourceFactory(book=True, source_publisher=None)
+
+    def test_save_article_journal_check(self) -> None:
+        with self.assertRaisesMessage(
+            ValueError, "Article is not linked to a journal!"
+        ):
+            SourceFactory(article=True, source_journal=None)
+
+    def test_save_article_publisher_check(self) -> None:
+        with self.assertRaisesMessage(
+            ValueError,
+            "Source with type 'article' has a publisher set! "
+            "(Should be done in the journal instead)",
+        ):
+            SourceFactory(article=True, source_publisher=PublisherFactory())
