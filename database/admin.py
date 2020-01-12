83c4040b326a84ac74b2ee511aa3c7ff88d115f4
Jasper Koops <jasperkoops91@gmail.com>
2020-01-12 11:26:06 +0100

    Expanded the model admin

--- database/admin.py
+++ database/admin.py
@@ -5,26 +5,41 @@ from database import models as database_models
 
 @admin.register(database_models.Author)
 class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "middle_name", "last_name"]
     list_filter = ["is_dummy_data"]
+    search_fields = ["first_name", "middle_name", "last_name"]
 
 
 @admin.register(database_models.Publisher)
 class PublisherAdmin(admin.ModelAdmin):
-    list_filter = ["is_dummy_data"]
+    list_display = ["name", "city"]
+    list_filter = ["is_dummy_data", "city"]
+    search_fields = ["name"]
 
 
 @admin.register(database_models.Journal)
 class JournalAdmin(admin.ModelAdmin):
-    list_filter = ["is_dummy_data"]
+    list_display = ["name", "journal_publisher"]
+    list_filter = ["is_dummy_data", "journal_publisher"]
+    search_fields = ["name"]
+    raw_id_fields = ["journal_publisher"]
 
 
 @admin.register(database_models.Source)
 class SourceAdmin(admin.ModelAdmin):
-    search_fields = ["title"]
+    list_display = [
+        "title",
+        "type",
+        "year_of_publication",
+        "source_publisher",
+        "source_journal",
+    ]
     list_filter = [
         "is_dummy_data",
+        "type",
         "authors",
         "source_publisher",
         "source_journal",
     ]
+    search_fields = ["title"]
     raw_id_fields = ["authors", "source_publisher", "source_journal"]
